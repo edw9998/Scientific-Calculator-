98ab96fa04b4719f786fa65089700697e74f0386
Edward <62472600+horbosis556@users.noreply.github.com>
2021-01-28 12:12:37 +0700

    Update graphs.py

--- VScodePython/graphs.py
+++ VScodePython/graphs.py
@@ -3,7 +3,6 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
-# Matplotlib With Tkinter Backend.
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 
 def plot_sin():
